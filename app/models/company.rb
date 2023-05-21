9e1866acac18140eeb1573e887c1a0fb5312b340
Sergio Oseko <osk.sergio@gmail.com>
2023-05-20 21:52:32 -0300

    feat: validates model company

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,5 @@
 class Company < ApplicationRecord
+  validades :corporate_name, :is_active, :email, presence: true
+  validades corporate_name, lenght: { maximum: 60 }
+  validades fantasy_name, lenght: { maximum: 40 }
 end
