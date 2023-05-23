164eff0a5b965ad63a6aa13d5f00e8ef3a328323
Sergio Oseko <osk.sergio@gmail.com>
2023-05-23 06:00:13 -0300

    feat: starting the implementation of the company controller

---
+++ app/controllers/companies_controller.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# controller companies
+class CompaniesController < ApplicationController
+  def index
+    companies = Company.all
+    render json: companies.select('id', 'corporate_name', 'fantasy_name', 'email', 'is_active'), status: :ok
+  end
+
+  def show
+    if Company.exists?(params[:id])
+      company = Company.find(params[:id])
+      render json: { corporate_name: company.corporate_name, fantasy_name: company.fantasy_name, 
+                     email: company.email, is_active: company.is_active }, status: :ok
+    else
+      render json: { status: 'Error', message: 'Company not found' }, status: :unprocessable_entity
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get 'companies/index', to: 'companies#index', as: 'index_company'
+  get 'companies/show/:id', to: 'companies#show', as: 'show_company'
 end
